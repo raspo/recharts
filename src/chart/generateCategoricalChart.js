9e164f434a9d7088173139c0c44267452c91c875
Tommaso <raspaccio@gmail.com>
2016-10-05 14:50:31 -0700

    add support for onClick on Tooltip

--- src/chart/generateCategoricalChart.js
+++ src/chart/generateCategoricalChart.js
@@ -583,6 +583,28 @@ const generateCategoricalChart = (ChartComponent, GraphicalChild) => {
         });
       }
     };
+
+    handleClickTooltip(offset, xAxisMap, yAxisMap, onClickTooltip, items, e) {
+      if (!onClickTooltip || !_.isFunction(onClickTooltip)) { return; }
+
+      const container = ReactDOM.findDOMNode(this);
+      const containerOffset = getOffset(container);
+      const ne = calculateChartCoordinate(e, containerOffset);
+      const mouse = this.getMouseInfo(xAxisMap, yAxisMap, offset, ne);
+
+      if (!mouse) { return; }
+
+      const { activeTooltipIndex, dataStartIndex, dataEndIndex } = this.state;
+      const data = this.props.data.slice(dataStartIndex, dataEndIndex + 1);
+
+      if (activeTooltipIndex < 0 || !items || !items.length
+        || activeTooltipIndex >= data.length) {
+        return;
+      }
+
+      onClickTooltip(data[activeTooltipIndex], activeTooltipIndex, e);
+    }
+
     /**
      * The handler of mouse entering chart
      * @param  {Object} offset   The offset of main part in the svg element
@@ -875,10 +897,13 @@ const generateCategoricalChart = (ChartComponent, GraphicalChild) => {
 
       const tooltipItem = findChildByType(children, Tooltip);
       const events = tooltipItem ? {
+        onClick: this.handleClickTooltip.bind(this, offset, xAxisMap, yAxisMap,
+          tooltipItem.props.onClick, items),
         onMouseEnter: this.handleMouseEnter.bind(this, offset),
         onMouseMove: this.handleMouseMove.bind(this, offset),
         onMouseLeave: this.handleMouseLeave,
       } : null;
+
       const attrs = getPresentationAttributes(others);
 
       return (
